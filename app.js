d0cd5b6771b4cda90cd60ee8be449735777e7c35
Ellyll <cawsllyffant-git@yahoo.com>
2020-08-30 20:51:59 +0100

    Load images using level data file

--- app.js
+++ app.js
@@ -3,29 +3,71 @@ document.addEventListener("DOMContentLoaded", function() {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
 
-    // context.fillStyle = '#FF0000';
-    // context.fillRect(0, 0, 250, 250);
-    // context.fill();
+    const entities = [
+        {
+            name: 'wall',
+            code: 'W',
+            size: { x: 16, y: 16},
+            src: 'img/wall.png'
+        },
+        {
+            name: 'floor',
+            code: 'F',
+            size: { x: 16, y: 16},
+            src: 'img/floor.png'
+        },
+        {
+            name: 'cheese',
+            code: 'C',
+            size: { x: 16, y: 16},
+            src: 'img/cheese_16x16.png'
+        }
+    ];
 
+    function renderEntity(entities, entityCode, x, y) {
+        const entity = entities.find(x => x.code == entityCode);
+        if (entity === undefined) {
+            console.log('ERROR', `Entity with code '${entityCode}' not found`);
+        }
+        else
+        {
+            context.drawImage(entity.img, x, y);
+        }
+    }
 
-    const wallImg = new Image(16,16);
-    wallImg.src = 'img/wall.png';
-    wallImg.onload = () => {
-        context.drawImage(wallImg, 0, 0);
-        context.drawImage(wallImg, 16, 0);
-        context.drawImage(wallImg, 32, 0);
-    };
+    let promises = [];
+    for(const element of entities) {
+        element.img = new Image(element.size.x, element.size.y);
+        element.img.src = element.src;
+        let promise = new Promise((resolve,reject) => {
+            element.img.onload = () => {
+                console.log('Loaded', element);
+                resolve();
+            }
+        });
+        promises.push(promise);
+    }
 
-    const floorImg = new Image(16,16);
-    floorImg.src = 'img/floor.png';
-    floorImg.onload = () => {
-        context.drawImage(floorImg, 0, 16);
-        context.drawImage(floorImg, 16, 16);
-        context.drawImage(floorImg, 32, 16);
-    };
-
-    const cheeseImg = new Image(16,16);
-    cheeseImg.src = 'img/cheese_16x16.png';
-    cheeseImg.onload = () => context.drawImage(cheeseImg, 0, 16);
+    Promise.all(promises).then(() => {
+        console.log("Loaded all");
+        fetch('level1.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                const NO_OF_LINES = 32;
+                const NO_OF_COLUMNS = 32;
+                const COLUMN_WIDTH = 16;
+                const LINE_HEIGHT = 16;
+                for (let line = 0 ; line < NO_OF_LINES ; line++ ) {
+                    for (let col = 0 ; col < NO_OF_COLUMNS ; col++) {
+                        const entityCode = data.data[line].charAt(col);
+                        const x = col * COLUMN_WIDTH;
+                        const y = line * LINE_HEIGHT;
+                        renderEntity(entities, entityCode, x, y);
+                    }
+                }
+            })
+            .catch((error) => console.log('Error:', error));
+    });
 
 });
